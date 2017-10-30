08fbe7980ece985beb2c84a2ec69b0582731ea65
elto <404088770@qq.com>
2017-10-30 16:58:21 +0800

    first commit

---
+++ src/main/java/ReverseInteger.java
@@ -0,0 +1,20 @@
+
+/**
+ * 7. Reverse digits of an integer.
+ * Example1: x = 123, return 321
+ * Example2: x = -123, return -321
+ */
+public class ReverseInteger {
+    public static int reverse(int x) {
+        long res = 0;
+        while(x != 0) {
+            res = res + x % 10;
+
+            // 检查是否有溢出
+            if(res > Integer.MAX_VALUE || res < Integer.MIN_VALUE) {
+                return 0;
+            }
+        }
+        return (int)res;
+    }
+}
